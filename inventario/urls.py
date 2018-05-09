aceb098b694dce63f878320b91bd10c5e12bd7fc
aosorio <argenisosorio580@hotmail.com>
2018-05-09 14:56:24 -0400

    Agregado el módulo de gestión de autenticación

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^', include('usuarios.urls', namespace='usuarios')),
     url(r'^', include('registro.urls', namespace='registro')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- registro/urls.py
+++ registro/urls.py
@@ -7,9 +7,9 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = patterns('',
-    url(r'^$', Index.as_view(), name='index'),
-    url(r'^consultar_bien$', views.Consultar_bien.as_view(), name='consultar_bien'),
-    url(r'^registrar_bien$', views.Registrar_bien.as_view(), name='registrar_bien'),
-    url(r'^editar_bien/(?P<pk>\d+)$', views.Editar_bien.as_view(), name='editar_bien'),
-    url(r'^borrar_bien/(?P<pk>\d+)$', views.Borrar_bien.as_view(), name='borrar_bien'),
+    url(r'^index$', login_required(Index.as_view()), name='index'),
+    url(r'^consultar_bien$', login_required(views.Consultar_bien.as_view()), name='consultar_bien'),
+    url(r'^registrar_bien$', login_required(views.Registrar_bien.as_view()), name='registrar_bien'),
+    url(r'^editar_bien/(?P<pk>\d+)$', login_required(views.Editar_bien.as_view()), name='editar_bien'),
+    url(r'^borrar_bien/(?P<pk>\d+)$', login_required(views.Borrar_bien.as_view()), name='borrar_bien'),
 )
