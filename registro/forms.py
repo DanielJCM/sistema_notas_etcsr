887595c967e20184f5fd3d7bb9b95642cc91ec20
DanielJCM <exodiadaniel@gmail.com>
2018-06-11 09:45:21 -0400

    Se cambio la tabla por una mas extensa

--- registro/forms.py
+++ registro/forms.py
@@ -154,6 +154,277 @@ class PlanillaForm(forms.ModelForm):
     nota_alumno10 = forms.CharField(widget=TextInput(attrs={
     }), required = False)
 
+    nombre_alumno11 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno11 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno11 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno12 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno12 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno12 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno13 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno13 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno13 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno14 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno14 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno14 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno15 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno15 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno15 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno16 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno16 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno16 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno17 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno17 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno17 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno18 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno18 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno18 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno19 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno19 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno19 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno20 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno20 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno20 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno21 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno21 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno21 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno22 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno22 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno22 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno23 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno23 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno23 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno24 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno24 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno24 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno25 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno25 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno25 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno26 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno26 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno26 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno27 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno27 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno27 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno28 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno28 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno28 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno29 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno29 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno29 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno30 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno30 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno30 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno31 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno31 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno31 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno32 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno32 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno32 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno33 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno33 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno33 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno34 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno34 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno34 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno35 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno35 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno35 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno36 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno36 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno36 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno37 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno37 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno37 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno38 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno38 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno38 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno39 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno39 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno39 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nombre_alumno40 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    cedula_alumno40 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    nota_alumno40 = forms.CharField(widget=TextInput(attrs={
+    }), required = False)
+
+    
     class Meta:
 
         model = Planilla
--- registro/models.py
+++ registro/models.py
@@ -8,10 +8,10 @@ class Planilla(models.Model):
     """
     Modelo de una planilla.
     """
-    campo0 = models.CharField(max_length=10000, blank=True,null=True)
-    campo1 = models.CharField(max_length=10000, blank=True,null=True)
-    campo2 = models.CharField(max_length=10000, blank=True,null=True)
-    campo3 = models.CharField(max_length=10000, blank=True,null=True)
+    campo0 = models.CharField(max_length=100, blank=True,null=True)
+    campo1 = models.CharField(max_length=100, blank=True,null=True)
+    campo2 = models.CharField(max_length=100, blank=True,null=True)
+    campo3 = models.CharField(max_length=100, blank=True,null=True)
     fecha = models.DateField(default=datetime.now, help_text='Fecha')
 
     # Campos de los alumnos
@@ -55,10 +55,127 @@ class Planilla(models.Model):
     cedula_alumno10= models.CharField(max_length=8, blank=True,null=True)
     nota_alumno10= models.CharField(max_length=2, blank=True,null=True)
 
+    nombre_alumno11= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno11= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno11= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno12= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno12= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno12= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno13= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno13= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno13= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno14= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno14= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno14= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno15= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno15= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno15= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno16= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno16= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno16= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno17= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno17= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno17= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno18= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno18= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno18= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno19= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno19= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno19= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno20= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno20= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno20= models.CharField(max_length=2, blank=True,null=True)
+    
+    nombre_alumno21= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno21= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno21= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno22= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno22= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno22= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno23= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno23= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno23= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno24= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno24= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno24= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno25= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno25= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno25= models.CharField(max_length=2, blank=True,null=True)
+    
+    nombre_alumno26= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno26= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno26= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno27= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno27= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno27= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno28= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno28= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno28= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno29= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno29= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno29= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno30= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno30= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno30= models.CharField(max_length=2, blank=True,null=True)
+    
+    nombre_alumno31= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno31= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno31= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno32= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno32= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno32= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno33= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno33= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno33= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno34= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno34= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno34= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno35= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno35= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno35= models.CharField(max_length=2, blank=True,null=True)
+    
+    nombre_alumno36= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno36= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno36= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno37= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno37= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno37= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno38= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno38= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno38= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno39= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno39= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno39= models.CharField(max_length=2, blank=True,null=True)
+
+    nombre_alumno40= models.CharField(max_length=100, blank=True,null=True)
+    cedula_alumno40= models.CharField(max_length=8, blank=True,null=True)
+    nota_alumno40= models.CharField(max_length=2, blank=True,null=True)
     def __unicode__(self):
-        return self.usuario    
-    def __unicode__(self):
-        return self.campo1
+        return self.campo0
 
     def get_absolute_url(self):
         return reverse('registro:editar_planilla', kwargs={'pk': self.pk})
